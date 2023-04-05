67890e80558bf76dbab2d9b0c185f7480deb86c5
李飞翰 <2021010645@secoder.net>
2023-04-05 21:27:25 +0800

    Add: add dataloader.py

---
+++ data_prepare/dataloader.py
@@ -0,0 +1,55 @@
+import scipy.io
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+class DataLoader:
+    '''
+    Function: 
+        Load data from ./dataset
+    Note:
+        N represents the number of neural cells, M represents the number of records at different times
+    Elements:
+        global_C(N, M): changes of calcium signals(0.1 seconds between adjacent elements)
+        global_S(N, M): The peak time of calcium activity in neuronal cells, a number other than 0 means that the activity of the neuron reaches its peak at this time.
+        global_centers(N ,2): the space location of neural cells
+        brain_region_id(N ,1): the number of brain regions where neurons are located
+        brain_region_name(N ,1): the name of brain regions where neurons are located
+
+        *_org: elements before clipping(infer_results_1 and infer_results_2 have different M, above elements use min(M1,M2))
+    '''
+
+    def __init__(self):
+        dataset_path = os.path.join(sys.path[0], "dataset")
+        self.infer_results_1 = self.load_mat(os.path.join(dataset_path, "infer_results_1.mat"))
+        self.infer_results_2 = self.load_mat(os.path.join(dataset_path, "infer_results_2.mat"))
+        self.infer_results_id_1 = self.load_mat(os.path.join(dataset_path, "infer_results_id_1.mat"))
+        self.infer_results_id_2 = self.load_mat(os.path.join(dataset_path, "infer_results_id_2.mat"))
+        self.brain_region_name_1 = self.load_excel(os.path.join(dataset_path, "Infer results 1/brain_region_name1.xlsx"))
+        self.brain_region_name_2 = self.load_excel(os.path.join(dataset_path, "Infer results 2/brain_region_name2.xlsx"))
+
+        cat = lambda x,y:np.concatenate((x,y),axis=0)
+        self.global_C = cat(self.infer_results_1["global_C"], self.infer_results_2["global_C"][:,:14122])
+        self.global_S = cat(self.infer_results_1["global_S"], self.infer_results_2["global_S"][:,:14122])
+        self.global_centers = cat(self.infer_results_1["global_centers"], self.infer_results_2["global_centers"])
+        self.brain_region_id = cat(self.infer_results_id_1["brain_region_id"], self.infer_results_id_2["brain_region_id"])
+        self.brain_region_name = cat(self.brain_region_name_1, self.brain_region_name_2)
+
+        self.global_C_org = [self.infer_results_1["global_C"], self.infer_results_2["global_C"]]
+        self.global_S_org = [self.infer_results_1["global_S"], self.infer_results_2["global_S"]]
+        self.global_centers_org = [self.infer_results_1["global_centers"], self.infer_results_2["global_centers"]]
+        self.brain_region_id_org = [self.infer_results_id_1["brain_region_id"], self.infer_results_id_2["brain_region_id"]]
+        self.brain_region_name_org = [self.brain_region_name_1, self.brain_region_name_2]
+
+    def load_mat(self, path):
+        print(f"Loading matlab file from {path}")
+        mat = scipy.io.loadmat(path)
+        return mat
+    
+    def load_excel(self, path):
+        print(f"Loading excel file from {path}")
+        dict = pd.read_excel(path)
+        first_element = dict.keys()[0]
+        return np.concatenate((np.array([first_element]).reshape(1,1), np.array(dict)),axis=0)
+
---
+++ main.py
@@ -0,0 +1,7 @@
+from data_prepare.dataloader import DataLoader 
+
+def main():
+    data = DataLoader()
+
+if __name__ == "__main__":
+    main()
